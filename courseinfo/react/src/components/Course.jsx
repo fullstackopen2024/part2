5d453b3298d403beef274c98ff5a6c803ae215fe
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-05-06 18:00:51 +0300

    [courseinfo][2.2] Solved exercise 2.2 courseinfo, step7

--- courseinfo/react/src/components/Course.jsx
+++ courseinfo/react/src/components/Course.jsx
@@ -2,10 +2,15 @@ import Content from "./Content.jsx";
 import Header from "./Header.jsx";
 
 const Course = ({course}) => {
+    function getTotalNumberOfExcecises(parts) {
+        return parts.reduce((subtotal, currentPart) => subtotal + currentPart.exercises, 0);
+    }
+
     return (
         <>
             <Header heading={course.name}/>
             <Content parts={course.parts}/>
+            <div>total of {getTotalNumberOfExcecises(course.parts)} exercises</div>
         </>
     )
 }
