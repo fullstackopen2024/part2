1966ed63442ec9b0cc482d4abc255875fc6bf1d0
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-05-07 23:25:27 +0300

    [phonebook][2.12] Solved exercise 2.12 phonebook, Step 7

--- phonebook/react/src/App.jsx
+++ phonebook/react/src/App.jsx
@@ -37,7 +37,12 @@ function App() {
             alert(`${newName} is already added to phonebook`)
             return;
         }
-        setPersons(persons.concat({name: newName, number: newNumber, id: persons.length + 1}))
+        const newPerson = {name: newName, number: newNumber};
+
+        axios.post("http://localhost:3001/persons", newPerson)
+            .then(response => setPersons(persons.concat(response.data)))
+        setNewName('')
+        setNewNumber('')
     }
 
     return (
